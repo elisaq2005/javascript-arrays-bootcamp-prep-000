aa30ff9e308e8a3c6cd181ae2aaa512404d98d58
elisaquiney@gmail.com <elisaquiney@gmail.com>
2018-03-25 02:44:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ var ingredient6 = "garlic"
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginingOfArray(array,element) {
+function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array,element) {
